3975e40e073d7abd85a294c1bfbb7fe26c0e94a9
Александр Хрусталев <returnXAA@outlook.com>
2020-06-19 14:54:08 +0500

    Добавьте файлы проекта.

---
+++ Lab2/ApplicationModel.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace Lab2
+{
+    class ApplicationModel : INotifyPropertyChanged
+    {
+        private string folder;
+        private string mask;
+        private string excludeMask;
+        private string find;
+        private string replace;
+        private SearchOption subdirectories;
+
+        public ApplicationModel()
+        {
+            folder = "";
+            mask = "";
+            excludeMask = "";
+            find = "";
+            replace = "";
+            subdirectories = SearchOption.TopDirectoryOnly;
+        }
+
+        public string Folder
+        {
+            get { return folder; }
+            set
+            {
+                folder = value;
+                OnPropertyChanged("Folder");
+            }
+        }
+
+        public string Mask
+        {
+            get { return mask; }
+            set
+            {
+                mask = value;
+                OnPropertyChanged("Mask");
+            }
+        }
+
+        public string ExcludeMask
+        {
+            get { return excludeMask; }
+            set
+            {
+                excludeMask = value;
+                OnPropertyChanged("ExcludeMask");
+            }
+        }
+
+        public string Find
+        {
+            get { return find; }
+            set
+            {
+                find = value;
+                OnPropertyChanged("Find");
+            }
+        }
+
+        public string Replace
+        {
+            get { return replace; }
+            set
+            {
+                replace = value;
+                OnPropertyChanged("Replace");
+            }
+        }
+
+        public bool SubDirectories
+        {
+            get
+            {
+                if (subdirectories == SearchOption.AllDirectories) return true;
+                else return false;
+            }
+            set
+            {
+                if (value) subdirectories = SearchOption.AllDirectories;
+                else subdirectories = SearchOption.TopDirectoryOnly;
+
+                OnPropertyChanged("SubDirectories");
+            }
+        }
+
+        public bool checkMask(string fileName)
+        {
+            string[] exts = excludeMask.Split('|', ',', ';');
+            string pattern = string.Empty;
+            foreach (string ext in exts)
+            {
+                pattern += @"^";
+                foreach (char symbol in ext)
+                    switch (symbol)
+                    {
+                        case '.': pattern += @"\."; break;
+                        case '?': pattern += @"."; break;
+                        case '*': pattern += @".*"; break;
+                        default: pattern += symbol; break;
+                    }
+                pattern += @"$|";
+            }
+            if (pattern.Length == 0) return false;
+            pattern = pattern.Remove(pattern.Length - 1);
+            Regex mask = new Regex(pattern, RegexOptions.IgnoreCase);
+            return mask.IsMatch(System.IO.Path.GetFileName(fileName));
+        }
+
+        public void FileContainsText(object sender, DoWorkEventArgs e)
+        {
+            string text;
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int count = 0;
+            List<string> result = new List<string>();
+            string[] allfiles = Directory.GetFiles(folder, (Mask.Equals("") ? "*" : Mask), (SearchOption)subdirectories);
+
+            List<string> files = new List<string>();
+            foreach (string temp in allfiles)
+                if (!checkMask(temp)) files.Add(temp);
+
+            double percent = ((double)1 / files.Count()) * 100;
+            double progressPercentage = 0;
+
+            foreach (string temp in files)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                string extention = Path.GetExtension(temp);
+                
+                    if (extention.Equals(".txt") || extention.Equals(".html") || extention.Equals(".css"))
+                    {
+                        text = File.ReadAllText(temp);
+                        if (text.Contains(find))
+                            result.Add(temp);
+                    }
+                    
+                count++;
+                progressPercentage += percent;
+                worker.ReportProgress((int)Math.Round(progressPercentage), $"Searching files contains text ({count} / {files.Count()})");
+            }
+
+            e.Result = result;
+            worker.DoWork -= FileContainsText;
+        }
+
+        public void ReplaceFiles(object sender, DoWorkEventArgs e)
+        {
+            string text;
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            FileContainsText(sender, e);
+
+            List<string> files = e.Result as List<string>;
+            List<string> result = new List<string>();
+
+            int count = 0;
+            double percent = ((double)1 / files.Count()) * 100;
+            double progressPercentage = 0;
+
+            foreach (string temp in files)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                string extention = Path.GetExtension(temp);
+                string newText;
+
+                    if (extention.Equals(".txt") || extention.Equals(".html") || extention.Equals(".css"))
+                    {
+                        text = File.ReadAllText(temp);
+                        newText = text.Replace(find, replace);
+                        File.WriteAllText(temp, newText);
+                    }
+
+                count++;
+                progressPercentage += percent;
+                worker.ReportProgress((int)Math.Round(progressPercentage), $"Replacing ({count} / {files.Count()})");
+            }
+
+            worker.DoWork -= ReplaceFiles;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Lab2/ApplicationViewModel.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+
+namespace Lab2
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private FolderBrowserDialog folderBrowserDialog;
+        private ApplicationModel applicationModel;
+
+        private ObservableCollection<string> goodFiles;
+        public ObservableCollection<string> GoodFiles
+        {
+            get { return goodFiles; }
+        }
+
+        public ApplicationModel AppModel
+        {
+            get { return applicationModel; }
+        }
+
+        public ProgressBar ProgBar
+        {
+            get { return progressBar; }
+        }
+
+        private BackgroundWorker worker;
+        private ProgressBar progressBar;
+
+        public ApplicationViewModel()
+        {
+            applicationModel = new ApplicationModel();
+            folderBrowserDialog = new FolderBrowserDialog();
+            progressBar = new ProgressBar();
+            worker = new BackgroundWorker();
+
+            goodFiles = new ObservableCollection<string>();
+        }
+
+        private RelayCommand find;
+        public RelayCommand Find
+        {
+            get
+            {
+                return find ??
+                    (find = new RelayCommand(obj =>
+                    {
+                        goodFiles.Clear();
+
+                        worker = new BackgroundWorker();
+                        worker.DoWork += applicationModel.FileContainsText;
+                        worker.RunWorkerCompleted += RunWorkerCompleted;
+                        worker.WorkerSupportsCancellation = true;
+                        worker.WorkerReportsProgress = true;
+                        worker.ProgressChanged += RunReportProgress;
+                        worker.RunWorkerAsync();
+
+                    }, obj => applicationModel.Folder.Any() && applicationModel.Find.Any() && !worker.IsBusy));
+            }
+        }
+
+        private RelayCommand replace;
+        public RelayCommand Replace
+        {
+            get
+            {
+                return replace ??
+                    (replace = new RelayCommand(obj =>
+                    {
+                        goodFiles.Clear();
+
+                        worker = new BackgroundWorker();
+                        worker.DoWork += applicationModel.ReplaceFiles;
+                        worker.RunWorkerCompleted += RunWorkerCompleted;
+                        worker.WorkerSupportsCancellation = true;
+                        worker.WorkerReportsProgress = true;
+                        worker.ProgressChanged += RunReportProgress;
+                        worker.RunWorkerAsync();
+
+                    }, obj => applicationModel.Folder.Any() && applicationModel.Find.Any() && applicationModel.Replace.Any() && !worker.IsBusy));
+            }
+        }
+
+        private RelayCommand selectFolder;
+        public RelayCommand SelectFolder
+        {
+            get
+            {
+                return selectFolder ??
+                    (selectFolder = new RelayCommand(obj =>
+                    {
+                        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                            applicationModel.Folder = folderBrowserDialog.SelectedPath;
+                    }));
+            }
+        }
+
+        private RelayCommand cancelOperation;
+        public RelayCommand CancelOperation
+        {
+            get
+            {
+                return cancelOperation ??
+                    (cancelOperation = new RelayCommand(obj =>
+                    {
+                        worker.CancelAsync();
+                    }, obj => worker != null && worker.IsBusy));
+            }
+        }
+
+        public void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled!");
+                progressBar.CurrentTask = "Cancelled";
+                progressBar.Progress = 0;
+            }
+            else
+            {
+                if (e.Error != null)
+                {
+                    MessageBox.Show("Something goes wrong!");
+                    progressBar.CurrentTask = "Error";
+                    progressBar.Progress = 0;
+                }
+                else
+                {
+                    List<string> files = e.Result as List<string>;
+
+                    foreach (string temp in files) goodFiles.Add(temp);
+                    progressBar.CurrentTask = $"Done! ({goodFiles.Count} / {goodFiles.Count})";
+                }
+            }
+        }
+
+        public void RunReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.CurrentTask = e.UserState.ToString();
+            progressBar.Progress = e.ProgressPercentage;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    public class ProgressBar : INotifyPropertyChanged
+    {
+        private int progress;
+        public int Progress
+        {
+            get { return progress; }
+            set
+            {
+                progress = value;
+                OnPropertyChanged("Progress");
+            }
+        }
+
+        private string currentTask;
+        public string CurrentTask
+        {
+            get { return currentTask; }
+            set
+            {
+                currentTask = value;
+                OnPropertyChanged("CurrentTask");
+            }
+        }
+
+        public ProgressBar()
+        {
+            progress = 0;
+            currentTask = "";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
